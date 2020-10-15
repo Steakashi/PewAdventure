915e9e0f8dfd7fb2001ea97d99540fd3d20c0ccd
Steakashi <newcacao@hotmail.fr>
2020-10-15 14:42:38 +0200

    feat: add basic movement (with simple animation) for main character

--- Assets/Scripts/Movements.cs
+++ Assets/Scripts/Movements.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 
 public class Movements : MonoBehaviour
@@ -8,88 +9,125 @@ public class Movements : MonoBehaviour
 
     public Rigidbody rb;
     public Camera cam;
+    public GameObject circle;
+    public GameObject arrow;
     public float force;
+    public float speedLimit;
+    
+    const float dragDistanceMax = .8f; // Correspond to a ratio multiplied by the narrower screen part size
+    const int circleTimeApparitionSpeed = 4;
 
-    const float force_factor = .5f;
+    private float squaredSpeedLimit;
+    private float dragTimeStart;
+  
 
     Vector3 click_position;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        squaredSpeedLimit = speedLimit * speedLimit;
     }
 
     // Update is called once per frame
     void Update()
     {
-        DisplayDebug();
+        
     }
 
-    Vector3 CalculateForceApplied()
+    Vector3 CalculateForceRotation()
     {
-        var camera_angle = Quaternion.AngleAxis(cam.transform.rotation.eulerAngles.y, Vector3.up);
-        var force_vector = new Vector3(Input.mousePosition.x - click_position.x, 0, Input.mousePosition.y - click_position.y);
-        return camera_angle * force_vector;
+        var cameraAngle = Quaternion.AngleAxis(cam.transform.rotation.eulerAngles.y, Vector3.up);
+        var forceVector = new Vector3(Input.mousePosition.x - click_position.x, 0, Input.mousePosition.y - click_position.y);
+
+        return (cameraAngle * forceVector * -1);
     }
 
-    void DisplayDebug()
+    float CalculateForceRatio()
     {
-        ///Debug.DrawRay(transform.position, Input.mousePosition - click_position, Color.red);
-        /*
-        Debug.DrawRay(transform.position,
-            new Vector3(
-            Input.mousePosition.x - click_position.x,
-            0,
-            0
-        ), Color.green);
 
-
-        Debug.DrawRay(transform.position,
-            new Vector3(
-            0,
-            0,
-            Input.mousePosition.y - click_position.y
-        ), Color.blue);*/
-
-        /*var new_vector = CalculateForceApplied();
-        Debug.DrawRay(transform.position, new_vector, Color.yellow);
-        //var final_vector = cam.transform.rotation * new_vector;
-        Debug.Log(cam.transform.rotation.eulerAngles.x);
-        Debug.Log(Quaternion.AngleAxis(cam.transform.rotation.eulerAngles.x, Vector3.up));
-
-        Vector3 final_vector = Quaternion.AngleAxis(cam.transform.rotation.eulerAngles.y, Vector3.up) * new_vector;
-        //final_vector = Quaternion.AngleAxis(cam.transform.rotation.eulerAngles.z, Vector3.right) * final_vector;
-
-        Debug.DrawRay(transform.position, final_vector, Color.cyan);
-
-
-
-        //Debug.DrawRay(transform.position, -CalculateForceApplied(), Color.green);*/
+        return Mathf.Clamp
+            (
+                (
+                    Vector2.Distance(Input.mousePosition, click_position) /
+                    (Mathf.Min(Screen.width, Screen.height) * .5f * dragDistanceMax)
+                ), 0, 1
+            );
     }
 
     void OnMouseDown()
     {
         // When user click on character for the first time, we store the mouse position for later calculations.
         click_position = Input.mousePosition;
+        dragTimeStart = Time.time;
 
     }
 
     void OnMouseDrag()
     {
         // Drag is beginning : at this point we juste display a visual element for displaying force level which will be applied later.
-        DisplayDebug();
+        //var screen_drag_ratio = Mathf.Clamp((Vector2.Distance(Input.mousePosition, click_position) / Mathf.Min(Screen.width, Screen.height) * .5f * dragDistanceMax), 0, 1);
+        //Debug.Log(Vector2.Distance(Input.mousePosition, click_position));
+        //Debug.Log(Mathf.Min(Screen.width, Screen.height) * .5f * dragDistanceMax);
+      
+        //Debug.Log(Mathf.Clamp((Vector2.Distance(Input.mousePosition, click_position) / (Mathf.Min(Screen.width, Screen.height) * .5f * dragDistanceMax))), 0, 1);
+       
+        var apparitionRatio = (Time.time - dragTimeStart) * circleTimeApparitionSpeed;
+        var forceRotation = CalculateForceRotation();
 
-        Vector3 difference = new Vector3(Input.mousePosition.x, 0, Input.mousePosition.y) - transform.position;
-        Quaternion goalRot = Quaternion.LookRotation(difference);
-        //transform.rotation = Quaternion.RotateTowards(transform.rotation, goalRot, 50);
+        circle.transform.localScale = Vector3.Lerp(Vector3.zero, Vector3.one, apparitionRatio);
+        arrow.transform.localScale = Vector3.Lerp(Vector3.zero, Vector3.one, Mathf.Min(CalculateForceRatio(), apparitionRatio));
+
+        if (forceRotation == Vector3.zero) { return; }
+        else
+        {
+            arrow.transform.rotation = Quaternion.LookRotation(forceRotation);
+            transform.rotation = Quaternion.RotateTowards(
+                transform.rotation,
+                Quaternion.LookRotation(forceRotation),
+                100
+            );
+        }
+
     }
 
     void OnMouseUp()
     {
-        
+
         // Player has released the mouse button : it is now time to launch the character.
         // Current mouse position is substract to previous recorded mouse position and clamped (following characters properties).
-        rb.AddForce(-CalculateForceApplied() * force_factor, ForceMode.Impulse);
+        circle.transform.localScale = Vector3.zero;
+        arrow.transform.localScale = Vector3.zero;
+
+        var forceApplied = CalculateForceRotation().normalized * force * CalculateForceRatio();
+
+        //Debug.Log(forceApplied.magnitude);
+
+       
+
+        if (rb.velocity.sqrMagnitude > squaredSpeedLimit) { return; }
+        else
+        {
+            if ((rb.velocity.sqrMagnitude + forceApplied.sqrMagnitude) > squaredSpeedLimit) {
+                /*
+                Debug.Log(forceApplied);
+                Debug.Log(forceApplied.sqrMagnitude);
+                Debug.Log(rb.velocity.sqrMagnitude);
+                Debug.Log(squaredForceLimit);
+                */
+                forceApplied = forceApplied * (float)((squaredSpeedLimit - rb.velocity.sqrMagnitude) / forceApplied.sqrMagnitude);
+                /*
+                Debug.Log(squaredSpeedLimit);
+                Debug.Log(rb.velocity.sqrMagnitude);
+                Debug.Log(forceApplied.sqrMagnitude);*/
+            }
+
+           //Debug.Log(forceApplied.magnitude);
+            rb.velocity = new Vector3();
+            rb.AddForce(forceApplied, ForceMode.Impulse);
+        }
+
+
+        //rb.velocity = Vector3.ClampMagnitude(rb.velocity, forceLimit);
     }
 }
