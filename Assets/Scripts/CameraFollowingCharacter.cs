43a24fba494dbe70ab6cda024f6d0069d5b2cace
Steakashi <newcacao@hotmail.fr>
2020-10-15 16:39:31 +0200

    feat: add simple camera that follows main player

---
+++ Assets/Scripts/CameraFollowingCharacter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowingCharacter : MonoBehaviour
+{
+
+    public Rigidbody mainPlayer;
+    public int speed;
+
+    private Vector3 base_position;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        base_position = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Slerp(transform.position, base_position + mainPlayer.transform.position, Time.deltaTime * speed);
+        
+        //transform.position = base_position * mainPlayer.transform.position;
+        //transform.Translate(mainPlayer.transform.position - base_position);
+        /*
+        transform.position = new Vector3(
+            mainPlayer.transform.position.x,
+            mainPlayer.transform.position.y,
+            mainPlayer.transform.position.z
+        );*/
+    }
+
+}
---
+++ Assets/Scripts/UIFollowingCharacter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIFollowingCharacter : MonoBehaviour
+{
+
+    public Rigidbody mainPlayer;
+
+    private RectTransform rect_transform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rect_transform = this.GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        rect_transform.position = new Vector3(
+            mainPlayer.transform.position.x,
+            mainPlayer.transform.position.y,
+            mainPlayer.transform.position.z
+        );
+    }
+
+}
