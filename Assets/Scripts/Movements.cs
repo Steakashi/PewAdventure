c7f8b93a494093e68cb195aa2d1c36a7858f8f42
Steakashi <newcacao@hotmail.fr>
2020-10-20 17:30:54 +0200

    feat: add dragBar

--- Assets/Scripts/Movements.cs
+++ Assets/Scripts/Movements.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using System;
 
 
@@ -13,12 +14,18 @@ public class Movements : MonoBehaviour
     public GameObject arrow;
     public float force;
     public float speedLimit;
-    
+    public float dragTime;
+
+    public Image dragBarUI;
+    public Gradient DragBarColor;
+
     const float dragDistanceMax = .8f; // Correspond to a ratio multiplied by the narrower screen part size
     const int circleTimeApparitionSpeed = 4;
 
     private float squaredSpeedLimit;
     private float dragTimeStart;
+    private float dragTimeEnd;
+    private bool canDrag = true;
   
 
     Vector3 click_position;
@@ -27,12 +34,16 @@ public class Movements : MonoBehaviour
     void Start()
     {
         squaredSpeedLimit = speedLimit * speedLimit;
+        dragTimeEnd = -dragTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var ratio = Mathf.Clamp((Time.time - dragTimeEnd) / 2, 0, 1);
+        dragBarUI.color = DragBarColor.Evaluate(ratio);
+        dragBarUI.transform.localScale = new Vector2(ratio, 1);
+        if (ratio == 1){ canDrag = true; }
     }
 
     Vector3 CalculateForceRotation()
@@ -65,13 +76,16 @@ public class Movements : MonoBehaviour
 
     void OnMouseDrag()
     {
+
+        if (!(canDrag)) { return; }
+
         // Drag is beginning : at this point we juste display a visual element for displaying force level which will be applied later.
         //var screen_drag_ratio = Mathf.Clamp((Vector2.Distance(Input.mousePosition, click_position) / Mathf.Min(Screen.width, Screen.height) * .5f * dragDistanceMax), 0, 1);
         //Debug.Log(Vector2.Distance(Input.mousePosition, click_position));
         //Debug.Log(Mathf.Min(Screen.width, Screen.height) * .5f * dragDistanceMax);
-      
+
         //Debug.Log(Mathf.Clamp((Vector2.Distance(Input.mousePosition, click_position) / (Mathf.Min(Screen.width, Screen.height) * .5f * dragDistanceMax))), 0, 1);
-       
+
         var apparitionRatio = (Time.time - dragTimeStart) * circleTimeApparitionSpeed;
         var forceRotation = CalculateForceRotation();
 
@@ -93,6 +107,8 @@ public class Movements : MonoBehaviour
 
     void OnMouseUp()
     {
+
+        if (!(canDrag)) { return; }
 
         // Player has released the mouse button : it is now time to launch the character.
         // Current mouse position is substract to previous recorded mouse position and clamped (following characters properties).
@@ -125,6 +141,9 @@ public class Movements : MonoBehaviour
            //Debug.Log(forceApplied.magnitude);
             rb.velocity = new Vector3();
             rb.AddForce(forceApplied, ForceMode.Impulse);
+
+            dragTimeEnd = Time.time;
+            canDrag = false;
         }
 
 
